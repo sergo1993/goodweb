50d0f4177440cb7911afe970c9413b4809048bbe
sergo1993 <potopnin.sergei@gmail.com>
2018-04-04 10:47:21 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html>
+<head lang="ru">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>GoodWeb</title>
+    <meta name="author" content="Potopnin Sergei"/>
+    <meta name="description" content="!!!!!"/>
+    <meta name="keywords" content="!!!!!"/>
+
+    <link rel="stylesheet" type="text/css" href="lib/css/bootstrap.css"/>
+    <link rel="stylesheet" type="text/css" href="lib/css/bootstrap-theme.css"/>
+    <link rel="stylesheet" type="text/css" href="lib/css/jquery.pagepiling.css"/>
+    <link rel="stylesheet" type="text/css" href="lib/css/pagepiling.css"/>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+</head>
+
+<body>
+<div id="p_prldr">
+    <img class="img_prelauder" src="/img/basic/preloader.png" id="rotateImg" alt="preloader">
+</div>
+
+<ul id="menu">
+    <li data-menuanchor="page1" class="active"><a id="link_page1" href="#page1"></a></li>
+    <li data-menuanchor="page2"><a id="link_page2" href="#page2"></a></li>
+    <li data-menuanchor="page3"><a id="link_page3" href="#page3"></a></li>
+    <li data-menuanchor="page4"><a id="link_page4" href="#page4"></a></li>
+    <li data-menuanchor="page5"><a id="link_page5" href="#page5"></a></li>
+</ul>
+
+<div id="pagepiling">
+    <div class="section" id="section1">
+        <nav>
+            <ul>
+                <li>
+                    <a class="section-1_link-website-development" href="/">
+                        <img src="/img/basic/website_development.png" alt="website-development">
+                        <span>Разработка<br>сайтов</span>
+                    </a>
+                    <img class="section-1_line-website-development" src="/img/basic/website_development_line.png"
+                         alt="line-website-development">
+                </li>
+                <li>
+                    <a class="section-1_link-about" href="/">
+                        <img src="/img/basic/about.png" alt="about">
+                        <span>О компании</span>
+                    </a>
+                    <img class="section-1_line-about" src="/img/basic/about_line.png" alt="about_line">
+                </li>
+                <li>
+                    <a class="section-1_link-promotion-sites" href="/">
+                        <img src="/img/basic/promotion_sites.png" alt="about">
+                        <span>Продвижение<br>сайтов</span>
+                    </a>
+                    <img class="section-1_line-promotion-sites" src="/img/basic/promotion_sites_line.png"
+                         alt="promotion_sites_line">
+                </li>
+                <li>
+                    <a class="section-1_link-publication" href="/">
+                        <img src="/img/basic/publication.png" alt="publication">
+                        <span>Статьи</span>
+                    </a>
+                    <img class="section-1_line-publication" src="/img/basic/publication_line.png"
+                         alt="publication_line">
+                </li>
+                <li>
+                    <a class="section-1_link-contact" href="/">
+                        <img src="/img/basic/contact.png" alt="contact">
+                        <span>Контакты</span>
+                    </a>
+                    <img class="section-1_line-contact" src="/img/basic/contact_line.png" alt="contact_line">
+                </li>
+                <li>
+                    <a class="section-1_link-portfolio" href="/">
+                        <img src="/img/basic/portfolio.png" alt="portfolio">
+                        <span>Портфолио</span>
+                    </a>
+                    <img class="section-1_line-portfolio" src="/img/basic/portfolio_line.png" alt="portfolio_line">
+                </li>
+            </ul>
+            <div class="logo"><a href="/"><img src="/img/basic/logo.png" alt="logo"></a></div>
+        </nav>
+
+    </div>
+    <div class="section" id="section2">
+        <header>
+            <h1 class="section-2_header">РАЗРАБОТКА И ПРОДВИЖЕНИЕ САЙТОВ ПОД КЛЮЧ</h1>
+        </header>
+        <p class="section-1_text">
+            <span class="text-indent"><a href="/">GoodWeb</a></span> – это команда квалифицированных специалистов в
+            области <a href="/">
+                разработки сайтов</a> с многолетним опытом работы
+            в городе Муроме, а также действующая по всей Владимирской области.
+            В последнее время имеет огромное значение тот факт, есть ли в наличии у компании свой сайт, на какой позиции
+            он находится и является ли он полезным и привлекательным для посетителей. Данный факт с каждым годом
+            становится все более и более актуальным, особенно для компаний, которые хотят иметь огромный успех в своей
+            сфере деятельности. Если же у компании нет личного сайта, то в глазах клиента авторитетность компании падает
+            и он, чаще всего, уходит к той компании, которая имеет свой сайт и проинформирует его в большем размере и
+            более удобной форме.<br>
+            <span class="text-indent">Наша</span> компания занимается <strong>деятельностью в сфере разработки и
+                продвижения сайтов в сети
+                интернет</strong> различной
+            сложности, что в будущем даст вам прирост потенциальных клиентов и дохода. Мы занимаемся разработкой таких
+            видов сайтов как landing page и визитки, корпоративные страницы, крупные CRM систем с выгрузкой данных на
+            множество торговых площадок, интернет магазины и многое другое, в зависимости от желания клиента. Ко всему
+            этому мы занимаемся выгрузкой сайта на хостинг, покупкой домена и дальнейшим <strong>обслуживанием и <a
+                    href="">продвижением
+                    сайта.</a></strong><br>
+            <span class="text-indent">При</span> разработке сайтов глубоко погружаемся в систему бизнеса заказчика,
+            выявляя ключевые моменты, на которые
+            необходимо делать упор для привлечения будущего клиента. Дизайн сайта подбирается в зависимости от желания
+            заказчика или мы предлагаем свои варианты и приходим к общему результату.
+        </p>
+    </div>
+    <div class="section" id="section3">
+        <section>
+            <h2 class="section3_header">ПОЧЕМУ СТОИТ ВЫБРАТЬ ИМЕННО НАС</h2>
+            <div class="col-lg-12">
+                <div class="section-3_item-advantage col-lg-3">
+                    <img src="/img/basic/individual_approach.png" alt="individual_approach">
+                    <p>Индивидуальный<br>подход</p>
+                </div>
+                <div class="section-3_item-advantage col-lg-3">
+                    <img src="/img/basic/favorable_price.png" alt="">
+                    <p>Выгодные<br>цены</p>
+                </div>
+                <div class="section-3_item-advantage col-lg-3">
+                    <img src="/img/basic/quality_work.png" alt="">
+                    <p>Качество<br>работы</p>
+                </div>
+                <div class="section-3_item-advantage col-lg-3">
+                    <img src="/img/basic/demanded_market.png" alt="">
+                    <p>Востребованы<br>на рынке</p>
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="section-3_item-advantage col-lg-3">
+                    <img src="/img/basic/long_experience.png" alt="">
+                    <p>Многолетний<br>опыт</p>
+                </div>
+                <div class="section-3_item-advantage col-lg-3">
+                    <img src="/img/basic/reliable_cooperation.png" alt="">
+                    <p>Надежное<br>сотрудничество</p>
+                </div>
+                <div class="section-3_item-advantage col-lg-3">
+                    <img src="/img/basic/constantly_develop.png" alt="">
+                    <p>Постоянно<br>развиваемся</p>
+                </div>
+                <div class="section-3_item-advantage col-lg-3">
+                    <img src="/img/basic/love_job.png" alt="">
+                    <p>Любим свою<br>работу</p>
+                </div>
+            </div>
+        </section>
+    </div>
+    <div class="section" id="section4">
+        <section>
+            <h2 class="section-4_header">НЕМНОГО СТАТИСТИКИ</h2>
+            <div class="col-lg-12">
+                <div class="col-lg-6">
+                    <p class="section-4_number">17</p>
+                    <p class="section-4_text">Сайтов сделано</p>
+                </div>
+                <div class="col-lg-6">
+                    <p class="section-4_number">9</p>
+                    <p class="section-4_text">Учавствовали в<br>разработке</p>
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="section-4_item-partner"></div>
+                <div class="section-4_item-partner"></div>
+                <div class="section-4_item-partner"></div>
+                <div class="section-4_item-partner"></div>
+            </div>
+        </section>
+    </div>
+    <div class="section" id="section5">
+        <section>
+            <form action="" method="post">
+                <input class="section-5_input-text" placeholder="Ваш e-Mail или телефон " name="name" type="text">
+                <input class="section-5_input-text" placeholder="Как к Вам обращаться" name="contact" type="text">
+                <input class="section-5_input-submit" type="submit" value="СВЯЗАТЬСЯ СО МНОЙ">
+            </form>
+        </section>
+        <footer>
+            <p class="copyright">© 2015 Rightblog.ru Copyright</p>
+        </footer>
+    </div>
+</div>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script type="text/javascript" src="lib/js/jquery.pagepiling.js"></script>
+<script type="text/javascript" src="lib/js/jqueryrotate.js"></script>
+<script type="text/javascript" src="lib/js/bootstrap.js"></script>
+<script type="text/javascript">
+    var deleteLog = false;
+    $(document).ready(function () {
+        $('#pagepiling').pagepiling({
+            menu: '#menu',
+            anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+            loopTop: true,
+            loopBottom: true
+        });
+    });
+</script>
+
+<script type="text/javascript">$(window).on('load', function () {
+        var angle = 0;
+        setInterval(function(){
+            angle+=3;
+            jQuery("#rotateImg").rotate(angle);
+        },50);
+
+        var $preloader = $('#p_prldr'),
+            $svg_anm   = $preloader.find('.svg_anm');
+        $svg_anm.fadeOut();
+        $preloader.delay(500).fadeOut('slow');
+        
+    });</script>
+
+</body>
+</html>
