07db77373e3cf981227f57a6807b4a90d2540bce
sergo1993 <potopnin.sergei@gmail.com>
2018-04-04 11:40:23 +0300

    ваф
    фвыа

--- index.php
+++ index.php
@@ -13,7 +13,12 @@
     <link rel="stylesheet" type="text/css" href="lib/css/pagepiling.css"/>
     <link rel="stylesheet" type="text/css" href="css/style.css"/>
 </head>
+<<<<<<< HEAD
 <body>хуило толстое
+=======
+хуйвввфвывфыфвыа
+<body>
+>>>>>>> parent of b1ad1a0... asdf
 <div id="p_prldr">
     <img class="img_prelauder" src="/img/basic/preloader.png" id="rotateImg" alt="preloader">
 </div>
